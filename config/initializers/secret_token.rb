fe27f6abec4386e79d2026872731618a0e4e79c8
Sem Ramos <alu0100603194@ull.edu.es>
2014-02-17 10:42:40 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P2rails::Application.config.secret_key_base = 'cf41af2eb3d7fa0fa6baade68ba3c508259199880bea1e2dab210fdfa290fa568be844781f7a64b5f4f571c4816ea4c5a1adee8e83d5874ab4721a2337ea89d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+P2rails::Application.config.session_store :cookie_store, key: '_p2rails_session'
